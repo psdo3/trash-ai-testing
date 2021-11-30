637b5ff2aaab8ca740e7560e00eb203a921fc530
Phu Do <phudo15@yahoo.com>
2021-11-30 14:48:46 -0600

    Removing duplicate

--- frontend-react/src/data/services/MessageService.js
+++ frontend-react/src/data/services/MessageService.js
@@ -1,10 +1,6 @@
 
 let mockMessages = [
     { id: 1, message : "Mock Message 1" } ,
-    { id: 2, message : "Mock Message 2" } ,
-    { id: 3, message : "Mock Message 3" } ,
-    { id: 4, message : "Mock Message 4" } ,
-    { id: 5, message : "Mock Message 5" }
 ];
 
 class MessageService {
