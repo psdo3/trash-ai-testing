e06498c3af79f2be47e461d570e44bea3b3dbb61
Brian Vasquez <alexandervasquez0082@gmail.com>
2021-11-30 20:09:02 -0600

    new user interface and the use of backend code

--- backend-nodejs/server.js
+++ backend-nodejs/server.js
@@ -1,12 +1,32 @@
 const express = require('express');
-
+const fileUpload = require('express-fileupload')
 const app = express();
 
-const PORT = 3000;
 
-app.use(express.static('public'));
-app.use('/images', express.static('images'));
+
+const PORT = 5000;
+
+//app.use(express.static('public'));
+//app.use('/images', express.static('images'));
+
+app.use(fileUpload());
+
+app.post('/upload', (req,res) =>{
+    if(req.files==null){
+        return res.status(400).json({msg: 'No file uploaded'});
+    }
+
+    const file = req.files.file;
+
+    file.mv(`${__dirname}/frontend-react/public/uploads/${file.name}`, err => {
+        if(err) {
+            console.error(err);
+            return res.status(500).send(err);
+        }
+        res.json({fileName: file.name, filePath: `/uploads/${file.name}`});
+    })
+});
 
 app.listen(PORT, () => {
-    console.log('Running server on port ${PORT}...');
+    console.log(`Server started on port ${PORT}...`);
 })
--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import FileUpload from './components/FileUpload';
+import './App.css';
 
-import MainTemplate from "./MainTemplate";
+const App = () => (
+  <div className='container mt-4'>
+    <h4 className='display-4 text-center mb-4'>
+      <i className='fab fa-react' /> React File Upload
+    </h4>
 
-class App extends React.Component {
-  render(){
-    return (
-        <MainTemplate />
-    );
-  }
-}
+    <FileUpload />
+  </div>
+);
 
 export default App;
+
