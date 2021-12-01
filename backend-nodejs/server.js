4b1a9caf1d8b81b7dc2f2ba24db43cb5b688d011
Phu Do <phudo15@yahoo.com>
2021-11-30 22:04:17 -0600

    Adjustment and travis

--- backend-nodejs/server.js
+++ backend-nodejs/server.js
@@ -11,19 +11,19 @@ const PORT = 5000;
 
 app.use(fileUpload());
 
-app.post('/upload', (req,res) =>{
+app.post('/images', (req,res) =>{
     if(req.files==null){
         return res.status(400).json({msg: 'No file uploaded'});
     }
 
     const file = req.files.file;
 
-    file.mv(`${__dirname}/uploads/${file.name}`, err => {
+    file.mv(`${__dirname}/images/${file.name}`, err => {
         if(err) {
             console.error(err);
             return res.status(500).send(err);
         }
-        res.json({fileName: file.name, filePath: `/uploads/${file.name}`});
+        res.json({fileName: file.name, filePath: `/images/${file.name}`});
     })
 });
 
